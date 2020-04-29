7dfc11c6eb0c18c79583ebaf3d843ea74978bb19
Bruno Vercelino da Hora <bruno.hora@pipefy.com>
2020-04-28 22:48:54 -0300

    Test: Adds a To-Do Item

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  getByText as getByTextInContainer,
+} from "@testing-library/react";
 import App from "../App";
 
 describe("<App />", () => {
   it("renders a To-Do input", () => {
     const { getByPlaceholderText } = render(<App />);
     expect(getByPlaceholderText("Add a to-do")).toBeInTheDocument();
+  });
+
+  it("adds a To-Do when inputing a text", () => {
+    const { getByPlaceholderText, getByTestId } = render(<App />);
+    fireEvent.change(getByPlaceholderText("Add a to-do"), {
+      target: { value: "Buy groceries" },
+    });
+    fireEvent.submit(getByPlaceholderText("Add a to-do"));
+    expect(
+      getByTextInContainer(getByTestId("todo-list"), "Buy groceries")
+    ).toBeInTheDocument();
   });
 });
