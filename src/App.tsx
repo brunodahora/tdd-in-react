039c883564592804b8b27b0ea332ec42465167b9
Bruno Vercelino da Hora <bruno.hora@pipefy.com>
2020-04-28 22:30:29 -0300

    Implementation: Show an input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div />;
+  return (
+    <div className="app">
+      <form>
+        <input type="text" placeholder="Add a to-do" className="input" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
