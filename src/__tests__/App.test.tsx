bc5feb8f441184dd28a2a4b2bf50a3bc0229d78e
Bruno Vercelino da Hora <bruno.hora@pipefy.com>
2020-04-28 23:26:54 -0300

    Test: Deletes a To-Do

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -35,4 +35,22 @@ describe("<App />", () => {
       "text-decoration: line-through"
     );
   });
+
+  it("deletes a To-Do", () => {
+    const {
+      getByPlaceholderText,
+      getByTestId,
+      getByText,
+      queryByText,
+    } = render(<App />);
+    fireEvent.change(getByPlaceholderText("Add a to-do"), {
+      target: { value: "Buy groceries" },
+    });
+    fireEvent.submit(getByPlaceholderText("Add a to-do"));
+    expect(
+      getByTextInContainer(getByTestId("todo-list"), "Buy groceries")
+    ).toBeInTheDocument();
+    fireEvent.click(getByText("x"));
+    expect(queryByText("Buy groceries")).toBeNull();
+  });
 });
